b0e1552f38e01493f1da227630cb28460dc7d957
Shyam Rana <shyamrana1590@gmail.com>
2025-12-31 21:25:55 +0530

    Add files via upload

---
+++ To-do List/script.js
@@ -0,0 +1,24 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+        <span onclick="toggleComplete(this)">${taskText}</span>
+        <button class="delete-btn" onclick="deleteTask(this)">Delete</button>
+      `;
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
+
+function deleteTask(button) {
+  const li = button.parentElement;
+  li.remove();
+}
+
+function toggleComplete(span) {
+  span.parentElement.classList.toggle("completed");
+}
